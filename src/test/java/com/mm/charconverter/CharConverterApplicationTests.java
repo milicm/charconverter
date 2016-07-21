3c77ee4f4939e7c99e7831231126a754bbbc75be
Milos Milic <mmilic@gmail.com>
2016-07-21 10:53:30 +0200

    Initial import

---
+++ src/test/java/com/mm/charconverter/CharConverterApplicationTests.java
@@ -0,0 +1,20 @@
+package com.mm.charconverter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.mm.charconverter.CharConverterApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CharConverterApplication.class)
+@WebAppConfiguration
+public class CharConverterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
