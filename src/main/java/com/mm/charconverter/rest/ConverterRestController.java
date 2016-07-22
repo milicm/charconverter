55f1ce2ad51054ab4de045892e21c9c17fef96cb
Milos Milic <mmilic@gmail.com>
2016-07-22 14:36:19 +0200

    Converter REST implementation

---
+++ src/main/java/com/mm/charconverter/rest/ConverterRestController.java
@@ -0,0 +1,35 @@
+package com.mm.charconverter.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mm.charconverter.domain.dto.ConverterObject;
+import com.mm.charconverter.exception.ConvTypeException;
+import com.mm.charconverter.service.ConverterService;
+import com.mm.charconverter.util.ConvType;
+import com.mm.charconverter.util.RestConst;
+
+@RestController
+@RequestMapping(RestConst.REST_CONVERTER_URL)
+public class ConverterRestController {
+
+	@Autowired
+	private ConverterService converterService;
+
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ConverterObject convert(@RequestBody(required = true) ConverterObject convObjReq) {
+		try {
+			ConvType convType = ConvType.findByType(convObjReq.getType());
+			String convertedText = converterService.convert(convObjReq.getText(), convType);
+			return new ConverterObject(HttpStatus.OK.value(), convObjReq.getType(), convObjReq.getText(), convertedText);
+		} catch (ConvTypeException ex) {
+			return new ConverterObject(HttpStatus.BAD_REQUEST.value(), convObjReq.getType(), convObjReq.getText(), ex.getMessage());
+		}
+	}
+}
