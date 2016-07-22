5a05cec468c3be12a63035469477a69a206f4b01
Milos Milic <mmilic@gmail.com>
2016-07-22 13:28:12 +0200

    Created findByType method

--- src/main/java/com/mm/charconverter/util/ConvType.java
+++ src/main/java/com/mm/charconverter/util/ConvType.java
@@ -1,5 +1,16 @@
 package com.mm.charconverter.util;
 
+import com.mm.charconverter.exception.ConvTypeException;
+
 public enum ConvType {
-	LAT_TO_CYR, CYR_TO_LAT
+	LAT_TO_CYR, CYR_TO_LAT;
+
+	public static ConvType findByType(String type) throws ConvTypeException {
+		for (ConvType t : ConvType.values()) {
+			if (t.toString().equals(type)) {
+				return t;
+			}
+		}
+		throw new ConvTypeException("Unknown conversion type!");
+	}
 }
