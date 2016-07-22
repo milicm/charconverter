3dc6f3d4231fef2dc2ef0c469498cf80d7153cd9
Milos Milic <mmilic@gmail.com>
2016-07-22 18:22:34 +0200

    Removed unused constant

--- src/main/java/com/mm/charconverter/util/RestConst.java
+++ src/main/java/com/mm/charconverter/util/RestConst.java
@@ -1,6 +1,5 @@
 package com.mm.charconverter.util;
 
 public class RestConst {
-	public static final String REST_CONVERTER_URL = "converter";
-	public static final String REST_CONVERT_URL = "convert";
+	public static final String REST_CONVERTER_URL = "/converter";
 }
