a6b5be305f5901a678ebfdff4224995be91e5b68
Milos Milic <mmilic@gmail.com>
2016-07-22 14:22:43 +0200

    Rest URL constants

---
+++ src/main/java/com/mm/charconverter/util/RestConst.java
@@ -0,0 +1,6 @@
+package com.mm.charconverter.util;
+
+public class RestConst {
+	public static final String REST_CONVERTER_URL = "converter";
+	public static final String REST_CONVERT_URL = "convert";
+}
